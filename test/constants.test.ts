c6da9c7ffec38d2725b70d0d57a7d1f1a167b36b
Oleksandr Dyomin <o.o.dyomin@gmail.com>
2020-11-11 02:45:58 +0200

    Use bytecode compiled by yarn (COMPUTED_INIT_CODE_HASH changes)

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,11 +1,11 @@
 import { INIT_CODE_HASH } from '../src/constants'
 
-import { bytecode } from '@eliteswap/v2-core/truffle_build_contracts/EliteswapV2Pair.json'
+import { bytecode } from '@eliteswap/v2-core/build/EliteswapV2Pair.json'
 import { keccak256 } from '@ethersproject/solidity'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
-const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [bytecode])
+const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
 
 describe('constants', () => {
   describe('INIT_CODE_HASH', () => {
