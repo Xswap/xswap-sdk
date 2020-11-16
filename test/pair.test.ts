1bfa1cd6922fa59852945f0f7cf249aa7955e5c4
Oleksandr Dyomin <o.o.dyomin@gmail.com>
2020-11-16 14:12:22 +0200

    Fix test

--- test/pair.test.ts
+++ test/pair.test.ts
@@ -14,7 +14,7 @@ describe('Pair', () => {
 
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      expect(Pair.getAddress(USDC, DAI)).toEqual('0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5')
+      expect(Pair.getAddress(USDC, DAI)).toEqual('0x3b4A3E6fA65704B2B2cE5552C9D2Bb51710CD403')
     })
   })
 
